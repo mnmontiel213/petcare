514a2e5f889eef9e25d012851bc77d6fea022a44
agusojeda <oagusj@gmail.com>
2025-04-23 12:37:34 -0300

    agrege compromiso y link de contacto a la principal

--- app/Views/contenido/principal.php
+++ app/Views/contenido/principal.php
@@ -1,17 +1,28 @@
 
 <!--- CAROUSEL --->
-<figure class="conteiner-banner">
-    <img class="banner" src="<?php echo base_url("assets/img/banner.svg") ?>" alt="banner">
-</figure>
-<div>
-    <div class="cs-img-huella1">
-        <img src="<?php echo base_url("assets/img/huella.svg") ?> " alt="">
-    </div>
-    <div class="cs-img-huella2">
-        <img src="<?php echo base_url("assets/img/huella.svg") ?> " alt="">
-    </div>
-    <div class="cs-img-huella3">
-        <img src="<?php echo base_url("assets/img/huella.svg") ?> " alt="">
-    </div>
+<div class="main-conteiner">
+    <figure class="conteiner-banner">
+        <img class="banner" src="<?php echo base_url("assets/img/banner.svg") ?>" alt="banner">
+    </figure>
+    <div class="main-content">
+        <div class="main-conpromiso">
+            <h2 class="title-compromiso">Compromiso con el Servicio</h2>
+            <p class="text-compromiso">En VetCare, creemos que cada mascota merece atención de calidad, cariño y respeto. Nuestro compromiso con el servicio va más allá del cuidado médico: nos esforzamos por crear un ambiente de confianza y cercanía con cada familia que nos elige.
 
+                Escuchamos, acompañamos y brindamos soluciones personalizadas, porque entendemos que cada animal es único y forma parte fundamental de tu vida. Estamos para vos, siempre que lo necesites.</p>
+            <div>
+            <a href="<?php echo base_url('contacto') ?>" class="btn-contacto">Contáctenos</a>
+            </div>
+        </div>
+        <div class="cs-img-huella1">
+            <img src="<?php echo base_url("assets/img/huella.svg") ?> " alt="">
+        </div>
+        <div class="cs-img-huella2">
+            <img src="<?php echo base_url("assets/img/huella.svg") ?> " alt="">
+        </div>
+        <div class="cs-img-huella3">
+            <img src="<?php echo base_url("assets/img/huella.svg") ?> " alt="">
+        </div>
+
+    </div>
 </div>
--- app/Views/plantillas/footer_view.php
+++ app/Views/plantillas/footer_view.php
@@ -1,56 +1,59 @@
-        <footer>
-            <br>
-            <div class="cs-footer">
+<footer class="cs-footer">
+    <div class="container py-4">
+        <div class="row text-center">
 
-                <div class="cs-footer-elements">
-                    <p>Ayuda</p>
-                    <ul>
-                        <li><a href="">Lorem impsum dolor sit</a></li>
-                        <li><a href="">Lorem impsum dolor sit</a></li>
-                        <li><a href="">Lorem impsum dolor sit</a></li>
-                        <li><a href="">Lorem impsum dolor sit</a></li>
-                        <li><a href="">Lorem impsum dolor sit</a></li>
-                    </ul>
-                </div>
-
-                <div class="cs-footer-elements">
-                    <p>Servicios</p>
-                    <ul>
-                        <li><a href="">Lorem impsum dolor sit</a></li>
-                        <li><a href="">Lorem impsum dolor sit</a></li>
-                        <li><a href="">Lorem impsum dolor sit</a></li>
-                        <li><a href="">Lorem impsum dolor sit</a></li>
-                        <li><a href="">Lorem impsum dolor sit</a></li>
-                    </ul>
-                </div>
-
-                <div class="cs-footer-elements">
-                    <p>Nosotros</p>
-                    <ul>
-                        <li><a href="">Lorem impsum dolor sit</a></li>
-                        <li><a href="">Lorem impsum dolor sit</a></li>
-                        <li><a href="">Lorem impsum dolor sit</a></li>
-                        <li><a href="">Lorem impsum dolor sit</a></li>
-                        <li><a href="">Lorem impsum dolor sit</a></li>
-                    </ul>
-                </div>
-
-                <div class="cs-footer-elements">
-                    <p>Contactos</p>
-                    <ul>
-                        <li><a href="">Lorem impsum dolor sit</a></li>
-                        <li><a href="">Lorem impsum dolor sit</a></li>
-                        <li><a href="">Lorem impsum dolor sit</a></li>
-                        <li><a href="">Lorem impsum dolor sit</a></li>
-                        <li><a href="">Lorem impsum dolor sit</a></li>
-                    </ul>
-                </div>
-
+            <div class="col-12 col-sm-6 col-md-3 cs-footer-elements">
+                <p class="cs-footer-title">Ayuda</p>
+                <ul class="p-0">
+                    <li><a href="#">Lorem ipsum dolor sit</a></li>
+                    <li><a href="#">Lorem ipsum dolor sit</a></li>
+                    <li><a href="#">Lorem ipsum dolor sit</a></li>
+                    <li><a href="#">Lorem ipsum dolor sit</a></li>
+                    <li><a href="#">Lorem ipsum dolor sit</a></li>
+                </ul>
             </div>
 
-            <h2>Fedora</h2>
+            <div class="col-12 col-sm-6 col-md-3 cs-footer-elements">
+                <p class="cs-footer-title">Servicios</p>
+                <ul class="p-0">
+                    <li><a href="#">Lorem ipsum dolor sit</a></li>
+                    <li><a href="#">Lorem ipsum dolor sit</a></li>
+                    <li><a href="#">Lorem ipsum dolor sit</a></li>
+                    <li><a href="#">Lorem ipsum dolor sit</a></li>
+                    <li><a href="#">Lorem ipsum dolor sit</a></li>
+                </ul>
+            </div>
 
-        </footer>
+            <div class="col-12 col-sm-6 col-md-3 cs-footer-elements">
+                <p class="cs-footer-title">Nosotros</p>
+                <ul class="p-0">
+                    <li><a href="#">Lorem ipsum dolor sit</a></li>
+                    <li><a href="#">Lorem ipsum dolor sit</a></li>
+                    <li><a href="#">Lorem ipsum dolor sit</a></li>
+                    <li><a href="#">Lorem ipsum dolor sit</a></li>
+                    <li><a href="#">Lorem ipsum dolor sit</a></li>
+                </ul>
+            </div>
+
+            <div class="col-12 col-sm-6 col-md-3 cs-footer-elements">
+                <p class="cs-footer-title">Contacto</p>
+                <ul class="p-0">
+                    <li><a href="#">Lorem ipsum dolor sit</a></li>
+                    <li><a href="#">Lorem ipsum dolor sit</a></li>
+                    <li><a href="#">Lorem ipsum dolor sit</a></li>
+                    <li><a href="#">Lorem ipsum dolor sit</a></li>
+                    <li><a href="#">Lorem ipsum dolor sit</a></li>
+                </ul>
+            </div>
+
+        </div>
+
+        <div class="mt-4">
+            <h2>Fedora</h2>
+        </div>
+    </div>
+</footer>
+
         <script src="<?php echo base_url("assets/js/bootstrap.bundle.min.js"); ?>"></script>
     </body>
 </html>
