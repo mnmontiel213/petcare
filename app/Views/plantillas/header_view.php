5b6231fe59c8ba4655a2150ce24d8e17a1628d0c
hetias <hetiasdou@gmail.com>
2025-05-08 14:47:53 -0300

    estructura y colores

--- app/Views/plantillas/header_view.php
+++ app/Views/plantillas/header_view.php
@@ -6,23 +6,15 @@
     <link rel="stylesheet" href=" <?php echo base_url("assets/css/bootstrap.min.css"); ?> ">
     <link href="https://cdn.jsdelivr.net/npm/bootstrap-icons@1.10.5/font/bootstrap-icons.css" rel="stylesheet">
     <link rel="stylesheet" href=" <?php echo base_url("assets/css/style.css"); ?> ">
-    <link rel="stylesheet" href=" <?php echo base_url("assets/css/nav.css"); ?> ">
-    <link rel="stylesheet" href=" <?php echo base_url("assets/css/principal.css"); ?> ">
-    <link rel="stylesheet" href=" <?php echo base_url("assets/css/contacto.css"); ?> ">
-    <link rel="stylesheet" href=" <?php echo base_url("assets/css/comercializacion.css"); ?> ">
-    <link rel="stylesheet" href=" <?php echo base_url("assets/css/terminos.css"); ?> ">
-    <link rel="stylesheet" href=" <?php echo base_url("assets/css/quienes_somos.css"); ?> ">
-    <link rel="stylesheet" href=" <?php echo base_url("assets/css/footer.css"); ?> ">
-
-
-    <link rel="stylesheet" href=" <?php echo base_url("assets/css/enDesarrollo.css"); ?> ">
 
     <!-- <script src="https://kit.fontawesome.com/8558fbf84a.js" crossorigin="anonymous"></script> -->
 
+    <!--
     <link rel="preconnect" href="https://fonts.googleapis.com">
     <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin>
     <link href="https://fonts.googleapis.com/css2?family=Playfair+Display:ital,wght@0,400..900;1,400..900&display=swap" rel="stylesheet">
     <script type="text/javascript" src="http://maps.google.com/maps/api/js?sensor=false"></script>
+    -->
     <title>Document</title>
 </head>
 <body onunload="GUnload()">
--- app/Views/plantillas/navbar_view.php
+++ app/Views/plantillas/navbar_view.php
@@ -1,30 +1,30 @@
 
 
-        <nav class="navbar navbar-expand-lg">
+        <nav class="navbar navbar-expand-lg custom-shadow" id="custom-navbar">
             <div class="container-fluid">
-                <a class="navbar-brand logo" href="<?php echo base_url('principal'); ?>">PetCare
-                    <img class="huella1" src="<?php echo base_url('assets/img/patas.png') ?>" alt="huella1">
-                    <img class="huella2" src="<?php echo base_url('assets/img/patas.png') ?>" alt="huella2">
-                </a>
-
                 <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarNavDropdown"
                     aria-controls="navbarNavDropdown" aria-expanded="false" aria-label="Toggle navigation">
                     <span class="navbar-toggler-icon"></span>
                 </button>
 
                 <div class="collapse navbar-collapse" id="navbarNavDropdown">
-                    <ul class="navbar-nav nav-list">
-                        <li class="nav-item">
-                            <a class="nav-link" aria-current="page" href="<?php echo base_url('comercializacion'); ?>">Comercialización</a>
+                    <ul class="navbar-nav nav-list custom-nav-elements-container">
+                        <li>
+                            <a class="navbar-brand custom-navbar-entry" href="<?php echo base_url('principal'); ?>">
+                                <img src=" <?php echo base_url("assets/img/PetCareLogo.png") ?> " alt="" style="width: 30%;">
+                            </a>
                         </li>
                         <li class="nav-item">
-                            <a class="nav-link" href="<?php echo base_url('catalogo'); ?>">Productos</a>
+                            <a class="nav-link custom-navbar-entry" aria-current="page" href="<?php echo base_url('comercializacion'); ?>">Servicios</a>
                         </li>
                         <li class="nav-item">
-                            <a class="nav-link" href="<?php echo base_url('quienes_somos'); ?>">Nosotros</a>
+                            <a class="nav-link custom-navbar-entry" href="<?php echo base_url('catalogo'); ?>">Productos</a>
+                        </li>
+                        <li class="nav-item">
+                            <a class="nav-link custom-navbar-entry" href="<?php echo base_url('quienes_somos'); ?>">Nosotros</a>
                         </li>
                         <li class="nav-item dropdown">
-                            <a class="nav-link dropdown-toggle" href="#" role="button" data-bs-toggle="dropdown"
+                            <a class="nav-link dropdown-toggle custom-navbar-entry" href="#" role="button" data-bs-toggle="dropdown"
                                 aria-expanded="false">
                                 Consultas
                             </a>
@@ -34,9 +34,9 @@
                                 <li><a class="dropdown-item" href="<?php echo base_url('consulta3'); ?>">Consulta 3</a></li>
                             </ul>
                         </li>
-                        <!-- <li class="nav-item">
+                        <!-- <li>
                             <a class="nav-link-img" href="<?php echo base_url(''); ?>"><img class="nav-img" src="<?php echo base_url("assets/img/comprar.png"); ?>" alt="carritoCompras"></a>   
-                        </li> -->
+                        </li> -->                            
                         <li class="nav-item">
                             <a class="nav-link-img" href="<?php echo base_url('principal'); ?>"><img class="nav-img-user" src="<?php echo base_url("assets/img/user.svg"); ?>" alt="login"></a>
                         </li>
