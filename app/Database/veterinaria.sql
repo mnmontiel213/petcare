61435a271c65577b6f3a81d2404abef3ed3da7dc
hetias <hetiasdou@gmail.com>
2025-05-16 17:48:10 -0300

    archivo de base de datos exportado desde phpmyadmin

--- app/Database/veterinaria.sql
+++ app/Database/veterinaria.sql
@@ -1,36 +1,109 @@
-CREATE DATABASE veterinaria;
+-- phpMyAdmin SQL Dump
+-- version 5.2.1
+-- https://www.phpmyadmin.net/
+--
+-- Host: localhost
+-- Generation Time: May 16, 2025 at 10:45 PM
+-- Server version: 10.4.32-MariaDB
+-- PHP Version: 8.2.12
 
-CREATE TABLE usuarios{
-    USUARIO_ID int NOT NULL AUTO_INCREMENT,
-    CBU int UNIQUE,
-    DNI int UNIQUE NOT NULL,
-    NOMBRE varchar(255) NOT NULL,
-    APELLIDO varchar(255) NOT NULL,
-    CORREO varchar(255) UNIQUE NOT NULL,
-    CONTRASEÑA varchar(255) NOT NULL,
-    DIRECCION varchar(255),
-    PRIMARY_KEY(USUARIO_ID)
-};
+SET SQL_MODE = "NO_AUTO_VALUE_ON_ZERO";
+START TRANSACTION;
+SET time_zone = "+00:00";
 
-CREATE TABLE productos{
-    CODIGO int,
-    NOMBRE varchar(255) NOT NULL,
-    MARCA varchar(255) NOT NULL,
-    PRECIO float NOT NULL,
-    PRIMARY_KEY(CODIGO)
-};
 
-CREATE TABLE consultas{
-    TITULO varchar(255) NOT NULL,
-    CORREO varchar(255) NOT NULL,
-    CONTENIDO text NOT NULL
-};
+/*!40101 SET @OLD_CHARACTER_SET_CLIENT=@@CHARACTER_SET_CLIENT */;
+/*!40101 SET @OLD_CHARACTER_SET_RESULTS=@@CHARACTER_SET_RESULTS */;
+/*!40101 SET @OLD_COLLATION_CONNECTION=@@COLLATION_CONNECTION */;
+/*!40101 SET NAMES utf8mb4 */;
 
-CREATE TABLE turnos{
-    TURNO_ID int AUTO_INCREMENT,
-    USUARIO_ID int NOT NULL,
-    FECHA date,
-    HORARIO time,
-    PRIMARY KEY (TURNO_ID),
-    FOREIGN KEY (USUARIO_ID) REFERENCES usuarios(USUARIO_ID)
-}
+--
+-- Database: `veterinaria`
+--
+
+CREATE DATABASE `veterinaria`;
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `consultas`
+--
+
+CREATE TABLE `consultas` (
+  `TITULO` varchar(128) NOT NULL,
+  `CORREO` text NOT NULL,
+  `CONTENIDO` text NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_unicode_ci;
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `productos`
+--
+
+CREATE TABLE `productos` (
+  `CODIGO` int(11) NOT NULL,
+  `NOMBRE` varchar(256) NOT NULL,
+  `MARCA` varchar(256) NOT NULL,
+  `PRECIO` float NOT NULL,
+  `PESO` float DEFAULT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_unicode_ci;
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `turnos`
+--
+
+CREATE TABLE `turnos` (
+  `TURNO_ID` int(11) NOT NULL,
+  `USUARIO_ID` int(11) NOT NULL,
+  `FECHA` date NOT NULL,
+  `HORARIO` time NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_unicode_ci;
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `usuarios`
+--
+
+CREATE TABLE `usuarios` (
+  `USUARIO_ID` int(11) NOT NULL,
+  `CBU` int(11) DEFAULT NULL,
+  `NOMBRE` varchar(256) NOT NULL,
+  `APELLIDO` varchar(256) NOT NULL,
+  `CORREO` varchar(256) NOT NULL,
+  `CONTRASEÑA` varchar(512) NOT NULL,
+  `DIRECCION` text DEFAULT NULL,
+  `ES_MAYORISTA` tinyint(1) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_unicode_ci;
+
+--
+-- Indexes for dumped tables
+--
+
+--
+-- Indexes for table `productos`
+--
+ALTER TABLE `productos`
+  ADD PRIMARY KEY (`CODIGO`);
+
+--
+-- Indexes for table `turnos`
+--
+ALTER TABLE `turnos`
+  ADD PRIMARY KEY (`TURNO_ID`);
+
+--
+-- Indexes for table `usuarios`
+--
+ALTER TABLE `usuarios`
+  ADD PRIMARY KEY (`USUARIO_ID`),
+  ADD UNIQUE KEY `CORREO` (`CORREO`),
+  ADD UNIQUE KEY `CBU` (`CBU`);
+COMMIT;
+
+/*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
+/*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;
+/*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
