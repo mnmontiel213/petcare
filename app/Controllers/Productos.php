086d25f0b8525b43c496f6965f053e8bcdb96215
hetias <hetiasdou@gmail.com>
2025-06-06 12:47:11 -0300

    pueden agregarse nuevos productos, categorias aun sin terminar

--- app/Controllers/Productos.php
+++ app/Controllers/Productos.php
@@ -12,19 +12,71 @@ class Productos extends BaseController{
         $result = $productoModel->findAll();
         
         $data = ['titulo' => "productos", 'productos' => $result];
-        return view('plantillas/header_view', $data).view('plantillas/navbar_view').view('contenido/productos/listar').view('plantillas/footer_view');
+        return view('plantillas/header_view', $data)
+            .view('plantillas/navbar_view')
+            .view('contenido/productos/listar')
+            .view('plantillas/footer_view');
     }
-
+    
     public function agregar(): string{
-        $data = ['titulo' => "agregar"];
-        return view('plantillas/header_view', $data).view('plantillas/navbar_view').view('contenido/productos/agregar').view('plantillas/footer_view');
+        $data = ['titulo' => "Agregar", 'validation' => null];
+        return view('plantillas/header_view', $data)
+            .view('plantillas/navbar_view')
+            .view('contenido/productos/agregar')
+            .view('plantillas/footer_view');
     }
 
     public function registrar_nuevo(): string{
-        
-        print_r($_POST);
+        $validation = \Config\Services::validation();
+        $request = \Config\Services::request();
 
-        if(isset($_POST['']))
+        $validation->setRules(
+            [
+                'nombre' => 'required',
+                'peso'   => 'required',
+                'precio' => 'required',
+                'categoria' => 'required',
+                //'imagen' => 'required',
+            ],
+            [
+                'nombre' =>[
+                    'required' => 'Ingrese un nombre'
+                ],
+                'peso' =>[
+                    'required' => 'Ingrese el peso'
+                ],
+                'precio' =>[
+                    'required' => 'Ingrese el precio'
+                ],
+                'categoria' =>[
+                    'required' => 'Ingrese la categoria'
+                ],
+            ]);
+
+        if($validation->withRequest($request)->run()){
+            $model = new ProductoModel();
+            
+            $model->save([
+                'CODIGO' => $this->request->getPost('codigo'),
+                'NOMBRE' => $this->request->getPost('nombre'),
+                'PRECIO' => $this->request->getPost('precio'),
+                'PESO' => $this->request->getPost('peso'),
+                'TIPO' => $this->request->getPost('categoria'),
+                'MASCOTA' => $this->request->getPost('mascota'),
+                'MARCA' => $this->request->getPost('marca'),
+            ]);
+            
+            return redirect->to('/');
+        }else{
+            // ERROR DE VALIDACION
+            $data['titulo'] = 'Agregar';
+            $data['validation'] = $validation->getErrors();
+
+            return view('plantillas/header_view', $data)
+            .view('plantillas/navbar_view')
+            .view('contenido/productos/agregar')
+            .view('plantillas/footer_view');            
+        }
         
         return "asdad";
     }
